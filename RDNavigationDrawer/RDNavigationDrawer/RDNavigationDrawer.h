efabad23915c41aa18ae1910e5532db269831833
greatrandz123 <dolfsupreme@gmail.com>
2019-04-03 06:56:50 +0800

    Set up navigation drawer framework

---
+++ RDNavigationDrawer/RDNavigationDrawer/RDNavigationDrawer.h
@@ -0,0 +1,19 @@
+//
+//  RDNavigationDrawer.h
+//  RDNavigationDrawer
+//
+//  Created by Randolf Dini-ay on 03/04/2019.
+//  Copyright © 2019 Randolf Dini-ay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RDNavigationDrawer.
+FOUNDATION_EXPORT double RDNavigationDrawerVersionNumber;
+
+//! Project version string for RDNavigationDrawer.
+FOUNDATION_EXPORT const unsigned char RDNavigationDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDNavigationDrawer/PublicHeader.h>
+
+
